392c41584f33bb26e4e92095981c5b1c749eca1f
Babarinde Oluwaseyi Abiodun <babarindeos@funaab.edu.ng>
2023-03-17 18:47:46 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Print single digits
+ * Return: 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+
+	printf("\n");
+
+	return (0);
+}
