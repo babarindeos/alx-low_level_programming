3e7637c8bcb5e83801111fb0f701b7cc39d1a664
Babarinde Oluwaseyi Abiodun <babarindeos@funaab.edu.ng>
2023-03-17 19:36:58 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Print all single digit combination
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+		{
+			continue;
+		}
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
