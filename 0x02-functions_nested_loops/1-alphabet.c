a057adef43fbec12ac8ecdbec5d434ecc4278839
Babarinde Oluwaseyi Abiodun <babarindeos@funaab.edu.ng>
2023-03-21 03:56:06 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Write a function that prints
+ * the alphabet in lowercase
+ * description: 1-alphabet.c
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
