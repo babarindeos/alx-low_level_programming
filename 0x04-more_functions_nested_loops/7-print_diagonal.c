82c97de7cf153de4e5a75bd08ddd518ac676e8d9
Babarinde Oluwaseyi Abiodun <babarindeos@funaab.edu.ng>
2023-03-23 16:26:19 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Print Diagonal lines
+ * @n: number of lines
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
