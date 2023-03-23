e1a619ffd0d14ac26444226307b62f265eee8810
Babarinde Oluwaseyi Abiodun <babarindeos@funaab.edu.ng>
2023-03-22 18:30:02 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Debugging  for positive or negative number
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negavtive(i);
+
+	return (0);
+}
