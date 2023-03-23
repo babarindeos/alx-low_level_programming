fb501df7f81adf281bff06e724642da167b73ce0
Babarinde Oluwaseyi Abiodun <babarindeos@funaab.edu.ng>
2023-03-22 18:55:32 -0700

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * positive_or_negative - Determine if a number is positive, negative or zero.
+ * @i: Number to be checked
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+	{
+		printf("%d is %s\n", i, "negative");
+	}
+	else if (i > 0)
+	{
+		printf("%d is %s\n", i, "positive");
+	}
+	else
+	{
+		printf("%d is %s\n", i, "zero");
+	}
+
+	return;
+}
