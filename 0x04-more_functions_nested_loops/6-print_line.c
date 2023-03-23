25e265f4210e63a54f69e094cfba5f383362dbc2
Babarinde Oluwaseyi Abiodun <babarindeos@funaab.edu.ng>
2023-03-23 15:56:30 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - Print line
+ * @n: length of line
+ * Return: void
+ */
+void print_line(int n)
+{
+	int counter;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (counter = 0; counter <= n; counter++)
+		{
+			_putchar('_');
+		}
+
+		_putchar('\n');
+	}
+}
