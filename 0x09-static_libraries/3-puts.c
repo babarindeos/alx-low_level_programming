fc0b61d4c6e28bb9ddb1235d9e1914a4246040f7
Babarinde Oluwaseyi Abiodun <babarindeos@funaab.edu.ng>
2023-04-14 09:04:20 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<stdio.h>
+/**
+* _puts - Entry point
+* @str: parameter
+* Return: void
+*/
+void _puts(char *str)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return;
+}
