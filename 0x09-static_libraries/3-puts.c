c4c663666cf69b951ca944996b276941a1c93ae3
Babarinde Oluwaseyi Abiodun <babarindeos@funaab.edu.ng>
2023-04-14 09:42:12 -0700

    libmy.a, main.h

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include<stdio.h>
-/**
-* _puts - Entry point
-* @str: parameter
-* Return: void
-*/
-void _puts(char *str)
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+void _puts(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return;
+	puts(s);
 }
