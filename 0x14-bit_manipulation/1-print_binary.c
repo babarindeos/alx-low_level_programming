10e36c31919ee26a4c69628dd4a35b7dce3c9d84
Babarinde Oluwaseyi A <babarindeos@funaab.edu.ng>
2023-05-04 16:14:19 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,29 +8,14 @@
 
 void print_binary(unsigned long int n)
 {
-	int i;
-	int count;
-	unsigned long int current;
-
-	count = 0;
-
-	for (i = 63; i >= 0; i--)
+	if (n >> 0)
 	{
-		current = n >> i;
-
-		if (current & 1)
-		{
-			_putchar(1);
-			count++;
-		}
-		else if (count)
-		{
-			_putchar('0');
-		}
-
-		if (!count)
-		{
-			_putchar('0');
-		}
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
 	}
 }
